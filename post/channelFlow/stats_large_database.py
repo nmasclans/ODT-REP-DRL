c350f0dada189c08e07e22e5a8ca036dc237ea17
nmasclans <nuria.masclans@upc.edu>
2023-06-30 16:10:00 +0200

    added comments

--- post/channelFlow/stats_large_database.py
+++ post/channelFlow/stats_large_database.py
@@ -39,11 +39,12 @@ Retau = 1.0/kvisc
 
 flist = sorted(gb.glob('../../data/'+caseN+'/data/data_00000/dmp_*.dat'))
 
-nunif  = int(1/dxmin)        # uniform grid (using smallest grid size)   
-nunif2 = int(nunif/2)        # half as many (for ploting to domain center)
+nunif  = int(1/dxmin)        # num. points uniform grid (using smallest grid size)   
+nunif2 = int(nunif/2)        # half of num. points (for ploting to domain center, symmetry in y-axis)
 
-nfiles = len(flist)
-yu  = np.linspace(-delta,delta,nunif)
+nfiles = len(flist)          # num. files of instantaneous data, i.e. num. discrete time instants
+yu  = np.linspace(-delta,delta,nunif) # uniform grid in y-axis
+# empty vectors of time-averaged quantities
 um  = np.zeros(nunif)        # mean velocity
 u2m = np.zeros(nunif)        # mean square velocity (for rms)
 vm  = np.zeros(nunif)
@@ -53,23 +54,26 @@ w2m = np.zeros(nunif)
 
 for ifile in flist :
 
-    data = np.loadtxt(ifile)
+    data = np.loadtxt(ifile);
     y = data[:,0]
     u = data[:,2]
     v = data[:,3]
     w = data[:,4]
 
-    uu = interp1d(y, u, fill_value='extrapolate')(yu)  # interpolate to uniform grid
+    # interpolate to uniform grid
+    uu = interp1d(y, u, fill_value='extrapolate')(yu)  
     vv = interp1d(y, v, fill_value='extrapolate')(yu)
     ww = interp1d(y, w, fill_value='extrapolate')(yu)
 
-    um += uu                 # update mean profiles
+    # update mean profiles
+    um += uu                 
     vm += vv
     wm += ww
     u2m += uu*uu
     v2m += vv*vv
     w2m += ww*ww
 
+# means
 um /= nfiles
 vm /= nfiles
 wm /= nfiles
@@ -77,6 +81,7 @@ um = 0.5*(um[:nunif2] + np.flipud(um[nunif2:]))  # mirror data (symmetric)
 vm = 0.5*(vm[:nunif2] + np.flipud(vm[nunif2:]))
 wm = 0.5*(wm[:nunif2] + np.flipud(wm[nunif2:]))
 
+# squared fluctuations
 u2m /= nfiles
 v2m /= nfiles
 w2m /= nfiles
@@ -84,16 +89,13 @@ u2m = 0.5*(u2m[:nunif2] + np.flipud(u2m[nunif2:]))
 v2m = 0.5*(v2m[:nunif2] + np.flipud(v2m[nunif2:]))
 w2m = 0.5*(w2m[:nunif2] + np.flipud(w2m[nunif2:]))
 
+# root-mean-squared fluctuations (rmsf)
 urms = np.sqrt(u2m - um*um)
 vrms = np.sqrt(v2m - vm*vm)
 wrms = np.sqrt(w2m - wm*wm)
 
 yu += delta         # domain center is at 0; shift so left side is zero
 yu = yu[:nunif2]    # plotting to domain center
-
-#####################################################################
-################     updated until here (nuria)      ################
-#####################################################################
 
 dudy = (um[1]-um[0])/(yu[1]-yu[0])
 utau = np.sqrt(kvisc * np.abs(dudy))
