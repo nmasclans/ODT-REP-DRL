4a52dafbc3111c2e3422798252b1bab47cb76297
nmasclans <credential.helper>
2023-12-14 11:07:23 +0100

    modify testTorch method to move tensor to CUDA GPU

--- source/reinforcementLearning.cc
+++ source/reinforcementLearning.cc
@@ -39,7 +39,32 @@ void reinforcementLearning::init(domain *p_domn) {
 */
 void reinforcementLearning::testTorch() {
   
-    torch::Tensor tensor = torch::rand({2, 3});
-    cout << tensor << endl;
+    // output main CUDA information
+    cout << "cuda is available: " << torch::cuda::is_available() << endl;
+    cout << "number of available gpus: " << torch:: cuda::device_count() << endl;
+
+    // check if CUDA (GPU) is available
+    if (torch::cuda::is_available()){
+        
+        // create a CUDA device
+        torch::Device device(torch::kCUDA);
+        // you could alternatively specify the targeted gpu, identified by an input variable gpu_id (int):
+        /*
+        if (gpu_id >= torch::cuda::device_count()) {
+            cout << "Invalid GPU ID. Using default GPU (ID 0)." << endl;
+            gpu_id = 0;
+        }
+        torch::Device device(torch::kCUDA, gpu_id);
+        */
+
+        // create & move tensor to the CUDA device
+        torch::Tensor tensor = torch::rand({2, 3}).to(device);
+
+        // print tensor (will be on GPU)
+        cout << tensor << endl;
+
+    } else {
+        cout << "CUDA is not available. Running on CPU." << endl;
+    }
 
 }
