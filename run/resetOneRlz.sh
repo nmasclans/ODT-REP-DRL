9c1da866d3d7eb3e727d64546db6cc608e9908a7
nmasclans <nuria.masclans@upc.edu>
2024-02-15 16:55:27 +0100

    new file for reset simulation

---
+++ run/resetOneRlz.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Single realization run
+# run as "./runOneRlz.sh" <caseName>
+# note it requires a single input argument: caseName
+
+###############################################################################
+echo "the start time is"
+date
+###############################################################################
+
+inputDir="../input/channelFlow/$1"
+caseName="$1"
+realizationNum="$2"
+tEndIncrement=1.0
+
+###############################################################################
+
+resetCase () {
+
+    cp     "$inputDir/"*        "../data/$caseName/input/" > /dev/null 2>&1
+
+    #--------------------------------------------------------------------------
+
+    echo "*** RESET, NEW REALIZATION ***"
+    echo "Output is being written to ../$caseName/runtime/runtime_* and ../$caseName/data"
+    ./odt.x $caseName $realizationNum          # realizationNum is the shift (realization # here)
+
+}
+
+###############################################################################
+
+resetCase
+
+###############################################################################
+echo
+echo "the end simulation time is"
+date
+###############################################################################
+
+exit 0
